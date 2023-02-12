b09639d708e12623cebb8bf48a25d150ab105f60
aguiarHugo <aguiarhugofx@gmail.com>
2023-02-12 20:28:46 -0300

    Created Sidebar

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { House, Bell, Hash, Envelope, BookmarkSimple, User, FileText, DotsThreeCircle } from 'phosphor-react'
+
+import twitterLogo from './assets/logo-twitter.svg'
+import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <div className="layout">
+      <aside className="sidebar">
+        <img className="logo" src={twitterLogo} alt="Logo" />
+
+        <nav className="main-navigation">
+          <a className="active" href=""> 
+            <House weight="fill" /> 
+            Home
+          </a>
+          <a href="">
+            <Hash />
+            Explore
+          </a>
+          <a href="">
+            <Bell />
+             Notifications
+          </a>
+          <a href="">
+            <Envelope /> 
+            Messages
+          </a>
+          <a href="">
+            <BookmarkSimple />
+            Bookmarks
+          </a>
+          <a href="">
+            <FileText />
+            Lists
+          </a>
+          <a href="">
+            <User />
+            Profile
+          </a>
+          <a href="">
+            <DotsThreeCircle />
+            More</a>
+        </nav>
+
+        <button className="new-tweet" type="button">Tweet</button>
+      </aside>
+      <div className="content"></div>
+    </div>
   </React.StrictMode>,
 )
