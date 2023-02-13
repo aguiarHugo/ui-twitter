dc39f4103d23e321837b1c10d653664ff6186d33
aguiarHugo <aguiarhugofx@gmail.com>
2023-02-13 14:40:21 -0300

    Reorganized CSS

--- src/main.tsx
+++ src/main.tsx
@@ -6,33 +6,15 @@ import { Tweet } from './components/Tweet'
 import { Sidebar } from './components/Sidebar'
 import { Header } from './components/Header'
 import { Separator } from './components/Separator'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
      <Sidebar />
       <div className="content">
-
-        <main className="timeline">
-          <Header title="Home" />
-
-          <form className="new-tweet-form">
-            <label htmlFor="tweet">
-              <img src="https://github.com/aguiarHugo.png" alt="Hugo Aguiar" />
-              <textarea id="tweet" placeholder="What's happening?"/>
-            </label>
-
-            <button type="submit">Tweet</button>
-          </form>
-
-          <Separator />
-
-          <Tweet/>
-          <Tweet/>
-          <Tweet/>
-          <Tweet/>
-
-        </main>
+        <RouterProvider router={router}/>
       </div>
     </div>
   </React.StrictMode>,
