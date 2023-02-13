2a565596bc682c830bad389e592fd9599fc02484
aguiarHugo <aguiarhugofx@gmail.com>
2023-02-13 20:24:01 -0300

    Added createNewTweet function

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from "react";
 import { Header } from "../components/Header";
 import { Separator } from "../components/Separator";
 import { Tweet } from "../components/Tweet";
@@ -10,16 +11,36 @@ const tweets = [
   'Deu certo!'
 ]
 
+let newTweet = ''
+
 export function Timeline(){
+  const [tweets, setTweets] = useState([
+    'Meu primeiro tweet',
+    'Teste',
+    'Deu certo!'
+  ])
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault()
+
+    setTweets([ newTweet, ...tweets])
+  }
+
   return(
     <main className="timeline">
           <Header title="Home" />
 
-
-          <form className="new-tweet-form">
+          <form onSubmit={createNewTweet} className="new-tweet-form">
             <label htmlFor="tweet">
               <img src="https://github.com/aguiarHugo.png" alt="Hugo Aguiar" />
-              <textarea id="tweet" placeholder="What's happening?"/>
+              <textarea 
+              id="tweet" 
+              placeholder="What's happening?"
+              value={newTweet}
+              onChange={(event) => {
+                newTweet = event.target.value
+              }}
+              />
             </label>
 
             <button type="submit">Tweet</button>
@@ -27,12 +48,9 @@ export function Timeline(){
 
           <Separator />
 
-          
-
           {tweets.map(tweet => {
-              return <Tweet key={tweet} content={tweet} />
-            }
-          )}
+              return <Tweet key={tweet} content={tweet} />  
+            })}
 
         </main>
   )
