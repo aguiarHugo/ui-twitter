55f6019aa34ad198eb440052cc5f2e1b4f5f9e57
aguiarHugo <aguiarhugofx@gmail.com>
2023-02-13 13:36:49 -0300

    Made Header a component

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,17 @@ import ReactDOM from 'react-dom/client'
 import './global.css'
 
 import { Tweet } from './components/Tweet'
-import { Sparkle } from 'phosphor-react'
 import { Sidebar } from './components/Sidebar'
+import { Header } from './components/Header'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
      <Sidebar />
       <div className="content">
+
         <main className="timeline">
-          <div className="timeline-header">
-            Home
-            <Sparkle />
-          </div>
+          <Header title="Home" />
 
           <form className="new-tweet-form">
             <label htmlFor="tweet">
