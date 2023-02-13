bc02f2b3ec9173baaf1a45cc9dd7dcaeeecca8b3
aguiarHugo <aguiarhugofx@gmail.com>
2023-02-13 08:26:02 -0300

    Made Sidebar a component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,47 @@
+import { Sparkle, House, Bell, Hash, Envelope, BookmarkSimple, User, FileText, DotsThreeCircle } from 'phosphor-react'
+import twitterLogo from '../assets/logo-twitter.svg'
+
+
+export function Sidebar() {
+  return (
+    <aside className="sidebar">
+    <img className="logo" src={twitterLogo} alt="Logo" />
+
+    <nav className="main-navigation">
+      <a className="active" href=""> 
+        <House weight="fill" /> 
+        Home
+      </a>
+      <a href="">
+        <Hash />
+        Explore
+      </a>
+      <a href="">
+        <Bell />
+         Notifications
+      </a>
+      <a href="">
+        <Envelope /> 
+        Messages
+      </a>
+      <a href="">
+        <BookmarkSimple />
+        Bookmarks
+      </a>
+      <a href="">
+        <FileText />
+        Lists
+      </a>
+      <a href="">
+        <User />
+        Profile
+      </a>
+      <a href="">
+        <DotsThreeCircle />
+        More</a>
+    </nav>
+
+    <button className="new-tweet" type="button">Tweet</button>
+  </aside>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,54 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Sparkle, House, Bell, Hash, Envelope, BookmarkSimple, User, FileText, DotsThreeCircle } from 'phosphor-react'
-import twitterLogo from './assets/logo-twitter.svg'
 import './global.css'
 
 import { Tweet } from './components/Tweet'
+import { Sparkle } from 'phosphor-react'
+import { Sidebar } from './components/Sidebar'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
-      <aside className="sidebar">
-        <img className="logo" src={twitterLogo} alt="Logo" />
-
-        <nav className="main-navigation">
-          <a className="active" href=""> 
-            <House weight="fill" /> 
-            Home
-          </a>
-          <a href="">
-            <Hash />
-            Explore
-          </a>
-          <a href="">
-            <Bell />
-             Notifications
-          </a>
-          <a href="">
-            <Envelope /> 
-            Messages
-          </a>
-          <a href="">
-            <BookmarkSimple />
-            Bookmarks
-          </a>
-          <a href="">
-            <FileText />
-            Lists
-          </a>
-          <a href="">
-            <User />
-            Profile
-          </a>
-          <a href="">
-            <DotsThreeCircle />
-            More</a>
-        </nav>
-
-        <button className="new-tweet" type="button">Tweet</button>
-      </aside>
-
+     <Sidebar />
       <div className="content">
         <main className="timeline">
           <div className="timeline-header">
@@ -71,7 +32,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
           <Tweet/>
           <Tweet/>
           <Tweet/>
-          
+
         </main>
       </div>
     </div>
